1fa862556703cd0101ef9a71bcf3a61666ab9671
haithamassoli <haitham.b.assoli@gmail.com>
2021-11-07 23:23:11 +0200

    added search

--- js/script.js
+++ js/script.js
@@ -3,22 +3,51 @@ const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
-menuBtn.onclick = ()=>{
+let searchData = document.querySelector(".search-data");
+let resultsBox = document.querySelector(".results-box");
+let resultsDiv = document.querySelector(".results-box div");
+// let searchData = document.querySelector(".search-data");
+let subArr = [];
+menuBtn.onclick = () => {
   items.classList.add("active");
   menuBtn.classList.add("hide");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
-}
-cancelBtn.onclick = ()=>{
+};
+cancelBtn.onclick = () => {
   items.classList.remove("active");
   menuBtn.classList.remove("hide");
   searchBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   form.classList.remove("active");
   cancelBtn.style.color = "#ff3d00";
-}
-searchBtn.onclick = ()=>{
+};
+searchBtn.onclick = () => {
   form.classList.add("active");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
+};
+console.log(subjects[0].name);
+searchData.addEventListener("keyup", (e) => {
+  const lowerData = e.target.value.toLowerCase();
+  subArr = subjects.filter((data) => {
+    return data.name.toLowerCase().includes(lowerData.toLowerCase());
+  });
+  subArr = subArr.map((e) => {
+    return `<li>${e.name}</li>`;
+  });
+  resultsBox.classList.add("active");
+  resultsDiv.classList.add("active");
+  showSub(subArr);
+});
+
+function showSub(list) {
+  let listData;
+  if (!list.length) {
+    lowerData = searchData.value;
+    list = lowerData;
+  } else {
+    listData = list.join("");
+  }
+  resultsDiv.innerHTML = listData;
 }
